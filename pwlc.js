4493680bc50f90f210f2cf72dbd6ba7a9c2353b6
Zachary Okuhara <zokuhara@gmail.com>
2015-12-01 14:41:05 -0600

    Functional weight tracking

---
+++ pwlc.js
@@ -0,0 +1,82 @@
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  Meteor.subscribe('thePlayers');
+  Template.leaderboard.helpers({
+    'player': function(){
+      var currentUserId = Meteor.userId();
+      return PlayersList.find({}, {sort: {score: -1, name: 1} })
+    },
+    'selectedClass': function(){
+      var playerId = this._id
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected"
+      }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer)
+    }
+  }); //end Template.leaderboard.helpers
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      var playerId = this._id;
+      Session.set('selectedPlayer', playerId);
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('modifyPlayerScore', selectedPlayer, 1);
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('modifyPlayerScore', selectedPlayer, -1)
+    },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      Meteor.call('removePlayerData', selectedPlayer);
+    }
+  }); //end Template.leaderboard.events
+
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      var playerScoreVar = event.target.playerScore.value;
+      var playerScoreInt = parseInt(playerScoreVar)
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: playerScoreVar,
+        createdBy: currentUserId
+      });
+      Meteor.call('insertPlayerData', playerNameVar, playerScoreInt);
+      template.find("addPlayerForm").reset();
+    }
+  });
+}
+if(Meteor.isServer){
+  Meteor.publish('thePlayers', function(){
+    var currentUserId = this.userId;
+    return PlayersList.find({createdBy: currentUserId})
+  });
+  Meteor.methods({
+    'insertPlayerData': function(playerNameVar, playerScoreVar){
+      var currentUserId = Meteor.userId();
+      PlayersList.insert({
+        name: playerNameVar,
+        score: playerScoreVar,
+        createdBy: currentUserId
+      });
+    },
+    'removePlayerData': function(selectedPlayer){
+      var currentUserId = Meteor.userId();
+      PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+    },
+    'modifyPlayerScore': function(selectedPlayer, scoreValue){
+      var currentUserId = Meteor.userId();
+      PlayersList.update( {_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score: scoreValue} });
+    }
+  }); //end Meteor.methods
+}
